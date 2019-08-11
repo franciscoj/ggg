4e2db543b0dea6d455adf8f155c8fa17c6274ffd
Fran Casas <nflamel@gmail.com>
2019-08-11 22:30:14 +0200

    feat: print unread notifications count

---
+++ src/credentials.rs
@@ -0,0 +1,13 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct GithubCredentials {
+  github_api_token: String
+}
+
+pub fn github_api_token() -> String {
+    match envy::from_env::<GithubCredentials>() {
+        Ok(creds) => creds.github_api_token,
+        Err(err) => panic!("{:#?}", err)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod credentials;
+
+use github_rs::client::{Executor, Github};
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let token = credentials::github_api_token();
+    let client = Github::new(token).unwrap();
+    let notifications = client.get().notifications().execute::<Value>();
+
+    match notifications {
+        Ok((_headers, _status, json)) => {
+            if let Some(json) = json {
+                if let Value::Array(arr) = json {
+                    // println!("{:#?}", arr);
+                    println!("Notifications: {}", arr.len());
+                }
+            }
+        }
+        Err(e) => println!("{}", e),
+    }
 }

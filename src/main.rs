0987ef7ec250aa36186ad5215237c9da75ba1eb2
Fran Casas <nflamel@gmail.com>
2019-08-12 22:10:19 +0200

    feat: use clap for CLI

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,26 @@ extern crate env_logger;
 use serde_json::Value;
 use std::collections::HashMap;
 
+use clap::{App, SubCommand};
+
 type JsonArray = Vec<Value>;
 
 fn main() {
     env_logger::init();
+
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+    let matches = App::new("ggg: A Github CLI")
+        .version(VERSION)
+        .author("Fran C. <nflamel@gmail.com>")
+        .about("Use Github web features from the CLI")
+        .subcommand(
+            SubCommand::with_name("notifications").about("Shows unread notifications count"),
+        )
+        .subcommand(
+            SubCommand::with_name("list_notifications")
+                .about("Shows unread notifications list per repo"),
+        )
+        .get_matches();
 
     let client = github::client().unwrap();
     let routes = github::v3::api_router::get_routes().unwrap();
@@ -18,22 +34,27 @@ fn main() {
 
     let mut resp = client.get(&notifications_url).send().unwrap();
     let nots: JsonArray = resp.json().unwrap();
-
     debug!("{:#?}", nots);
     info!("{:#?}", resp);
 
-    let mut repos_map: HashMap<String, i32> = HashMap::new();
-
-    for n in nots.iter() {
-        let repo_name = n["repository"]["full_name"].to_string();
-
-        let nots_count = repos_map.entry(repo_name).or_insert(0);
-        *nots_count += 1;
+    if let Some(_matches) = matches.subcommand_matches("notifications") {
+        println!("{}", nots.len());
     }
 
-    println!("Total notifications: {}", nots.len());
+    if let Some(_matches) = matches.subcommand_matches("list_notifications") {
+        let mut repos_map: HashMap<String, i32> = HashMap::new();
 
-    for (repo, nots_count) in &repos_map {
-        println!("Notifications {}: {}", repo, nots_count);
+        println!("Total notifications: {}", nots.len());
+
+        for n in nots.iter() {
+            let repo_name = n["repository"]["full_name"].to_string();
+
+            let nots_count = repos_map.entry(repo_name).or_insert(0);
+            *nots_count += 1;
+        }
+
+        for (repo, nots_count) in &repos_map {
+            println!("Notifications {}: {}", repo, nots_count);
+        }
     }
 }

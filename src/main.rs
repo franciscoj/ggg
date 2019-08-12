452ade83797a2217b999ceb2355816991a8bf863
Fran Casas <nflamel@gmail.com>
2019-08-12 14:39:45 +0200

    feat: list notifications per repo

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate env_logger;
 
 use github_rs::client::{Executor, Github};
 use serde_json::Value;
+use std::collections::HashMap;
+
+type JsonArray = Vec<Value>;
 
 fn main() {
     env_logger::init();
@@ -13,21 +16,34 @@ fn main() {
     let token = credentials::github_api_token();
     let client = Github::new(token).unwrap();
 
-    // TODO: This can for sure be improved to use a better type than `Value`
-    let notifications = client.get().notifications().execute::<Value>();
+    let (headers, status, json) = client.get().notifications().execute::<JsonArray>().unwrap();
 
-    match notifications {
-        Ok((headers, status, json)) => {
-            info!("Status: {:#?}", status);
-            info!("Headers: {:#?}", headers);
+    debug!("Status: {:#?}", status);
+    debug!("Headers: {:#?}", headers);
 
-            if let Some(json) = json {
-                if let Value::Array(arr) = json {
-                    debug!("{:#?}", arr);
-                    println!("Notifications: {}", arr.len());
-                }
-            }
+    if let Some(nots) = json {
+        debug!("{:#?}", nots);
+        let mut repos_map: HashMap<String, i32> = HashMap::new();
+
+        for n in nots.iter() {
+            let repo_name = n["repository"]["full_name"].to_string();
+
+            let nots_count = repos_map.entry(repo_name).or_insert(0);
+            *nots_count += 1;
+
+            info!(
+                "[{repo} | {reason}] {title}",
+                repo = n["repository"]["full_name"],
+                reason = n["reason"],
+                title = n["subject"]["title"]
+            );
         }
-        Err(e) => println!("{}", e),
+
+        debug!("{:#?}", nots[0]);
+
+        println!("Total notifications: {}", nots.len());
+        for (repo, nots_count) in &repos_map {
+            println!("Notifications {}: {}", repo, nots_count);
+        }
     }
 }

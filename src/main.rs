eafb58e31b8d2f1dd06f473cb63c8b0093bd7d3a
Fran Casas <nflamel@gmail.com>
2019-08-12 11:07:02 +0200

    feat: add logs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 mod credentials;
 
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 use github_rs::client::{Executor, Github};
 use serde_json::Value;
 
 fn main() {
+    env_logger::init();
+
     let token = credentials::github_api_token();
     let client = Github::new(token).unwrap();
+
+    // TODO: This can for sure be improved to use a better type than `Value`
     let notifications = client.get().notifications().execute::<Value>();
 
     match notifications {
-        Ok((_headers, _status, json)) => {
+        Ok((headers, status, json)) => {
+            info!("Status: {:#?}", status);
+            info!("Headers: {:#?}", headers);
+
             if let Some(json) = json {
                 if let Value::Array(arr) = json {
-                    // println!("{:#?}", arr);
+                    debug!("{:#?}", arr);
                     println!("Notifications: {}", arr.len());
                 }
             }

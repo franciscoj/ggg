4d0143ae6c15a5548d35c5bde0212300fd457b66
Fran Casas <nflamel@gmail.com>
2019-08-13 21:38:01 +0200

    test: for gh v3 api router

--- src/github/v3/api_router.rs
+++ src/github/v3/api_router.rs
@@ -11,3 +11,18 @@ pub fn get_routes() -> Result<Endpoints, Error> {
 
     serde_json::from_str(&routes)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_returns_notifications_url() {
+        let routes = get_routes().unwrap();
+
+        assert_eq!(
+            routes.notifications_url,
+            "https://api.github.com/notifications"
+        )
+    }
+}

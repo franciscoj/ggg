745f7444e1a8704d2ab07ac119ec4d5f002b8046
Fran Casas <nflamel@gmail.com>
2020-01-05 10:25:06 +0100

    refactor: better variable names

--- src/commands/list_notifications.rs
+++ src/commands/list_notifications.rs
@@ -10,18 +10,18 @@ pub fn cmd() {
     let notifications_url = routes.notifications_url;
     let req = Request::new(notifications_url);
     let nots = req.fetch_all(&client);
-    let mut repos_map: HashMap<String, i32> = HashMap::new();
+    let mut repos_by_name: HashMap<String, i32> = HashMap::new();
 
     println!("Total notifications: {}", nots.len());
 
     for n in nots.iter() {
         let repo_name = n["repository"]["full_name"].to_string();
 
-        let nots_count = repos_map.entry(repo_name).or_insert(0);
+        let nots_count = repos_by_name.entry(repo_name).or_insert(0);
         *nots_count += 1;
     }
 
-    for (repo, nots_count) in &repos_map {
+    for (repo, nots_count) in &repos_by_name {
         println!("Notifications {}: {}", repo, nots_count);
     }
 }

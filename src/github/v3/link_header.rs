19d5cbd6f406ebacd5ff0f13b33a0dfdce43d25f
Fran Casas <nflamel@gmail.com>
2020-01-05 08:12:46 +0100

    test: empty link header

--- src/github/v3/link_header.rs
+++ src/github/v3/link_header.rs
@@ -16,10 +16,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_returns_link_url() {
+    fn it_parses_link_url() {
         let link = "<https://api.github.com/notifications?page=2>; rel=\"next\", <https://api.github.com/notifications?page=2>; rel=\"last\"";
         let url = parse(link).unwrap();
 
         assert_eq!(url, "https://api.github.com/notifications?page=2")
+    }
+
+    #[test]
+    fn it_parses_empty_link() {
+        let link = "";
+        let url = parse(link);
+
+        assert_eq!(url, None)
     }
 }
